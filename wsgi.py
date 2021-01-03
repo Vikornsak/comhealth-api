f4a3556b18ed5a9fb0e32b3090b228e9700bc0a7
Likit Preeyanon <likit.pre@mahidol.edu>
2021-01-04 01:47:29 +0700

    use waitress as a server
    added a blueprint for API app

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,9 @@
 from app import create_app
 from app.apis import apis
+from waitress import serve
 
 app = create_app()
 
 app.register_blueprint(apis, url_prefix='/apis')
+
+serve(app, host='0.0.0.0', port=8080, threads=1)
