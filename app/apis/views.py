6f06db15c592d9056ad7389b8fe9fc0db922e217
Likit Preeyanon <likit.pre@mahidol.edu>
2021-05-19 17:44:33 +0700

    added code that connects to remote sql server
    added a view that fetches tests from the database

--- app/apis/views.py
+++ app/apis/views.py
@@ -1,6 +1,11 @@
+import pandas as pd
+from flask import jsonify
+
 from . import apis
 from app import engine
 
+
 @apis.route('/tests')
 def get_tests():
-    return 'hello, world'
+    df = pd.read_sql_table('Test', con=engine)
+    return jsonify(df.head().to_dict())
