69052b044fdf202b5e65149592ea43fd185a30e8
genocide20 <changesak@gmail.com>
2022-04-01 12:25:39 +0700

    Update package and Add development mode

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,13 @@
 from app import create_app
 from app.apis import apis
 from waitress import serve
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 app = create_app()
 
 app.register_blueprint(apis, url_prefix='/apis')
 
-serve(app, host='0.0.0.0', port=8080, threads=1)
+if os.environ.get("FLASK_ENV") != "development":
+    serve(app, host='0.0.0.0', port=8080, threads=1)
