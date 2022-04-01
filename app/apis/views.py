7a50754399b2ffc644d6a70fcfae0b25a6379750
Likit Preeyanon <likit.pre@mahidol.edu>
2022-04-01 11:31:51 +0700

    updated the views

--- app/apis/views.py
+++ app/apis/views.py
@@ -1,11 +1,37 @@
+import csv
+from io import StringIO
+
 import pandas as pd
-from flask import jsonify
+from flask import jsonify, Response, request
 
 from . import apis
 from app import engine
 
 
+def generate(df):
+    data = StringIO()
+    w = csv.writer(data)
+
+    # write header
+    w.writerows(df.to_csv())
+    return data.getvalue()
+
+
 @apis.route('/tests')
 def get_tests():
     df = pd.read_sql_table('Test', con=engine)
-    return jsonify(df.head().to_dict())
+    return jsonify(df.to_dict())
+
+
+@apis.route('/tests-by-month')
+def get_tests_by_month():
+    df = pd.read_sql_query("select ServiceDate,TCode, count(*) as 'Counts' from Lab where ServiceDate>='2022-01-01' group by ServiceDate,TCode order by ServiceDate;",
+                           con=engine)
+    return jsonify(df.to_dict())
+
+
+@apis.route('/customers-by-month')
+def get_customers_by_month():
+    df = pd.read_sql_query("select count(*) as 'NumberCustomer', ServiceDate from Services where ServiceDate>='2021-01-01' group by ServiceDate;",
+                           con=engine)
+    return jsonify(df.to_dict())
