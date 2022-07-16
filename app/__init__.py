222449f031aa1421853cb7693bf038e52f66c29b
Likit Preeyanon <likit.pre@mahidol.edu>
2022-07-16 11:47:29 +0700

    installed flask-marshmallow, marshmallow-sqlalchemy
    created schemas for services, results and customer related models with appropriate relationships for ease of accessing the results
    
    moved some app initialization code to the main app module

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,31 @@
-from flask import Flask
-from sqlalchemy import create_engine
+import os
 
-engine = create_engine('mssql+pyodbc://10.4.101.16/cmsLIS?driver=SQL+Server+Native+Client+11.0')
+from flask import Flask
+from waitress import serve
+from dotenv import load_dotenv
+from sqlalchemy.ext.automap import automap_base
+from app.extensions import db, ma
+
+
+# engine = create_engine('mssql+pyodbc://10.4.101.16/cmsLIS?driver=SQL+Server+Native+Client+11.0')
+
 
 def create_app():
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.environ.get('FLASK_SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
+    db.init_app(app)
+    ma.init_app(app)
     return app
+
+
+load_dotenv()
+app = create_app()
+with app.app_context():
+    Base = automap_base()
+    Base.prepare(db.engine, reflect=True)
+
+from app.apis import apis
+
+app.register_blueprint(apis, url_prefix='/apis')
--- app/apis/views.py
+++ app/apis/views.py
@@ -2,10 +2,40 @@ import csv
 from io import StringIO
 
 import pandas as pd
-from flask import jsonify, Response, request
+from flask import jsonify
+from sqlalchemy.orm import relationship
 
+from app import Base
 from . import apis
-from app import engine
+from app.extensions import db
+from app.apis.schemas import *
+
+
+@apis.route('/v1.0/customer/<int:cmsCode>')
+def get_customer_profile(cmsCode):
+    employee = db.session.query(Employee).get(cmsCode)
+    print(employee.services)
+    return jsonify({'data': employee_schema.dumps(employee)})
+
+
+@apis.route('/v1.0/service/<int:service_no>')
+def get_service(service_no):
+    service = db.session.query(Services).get(service_no)
+    return jsonify({'data': service_schema.dumps(service)})
+
+
+@apis.route('/labs')
+def get_labs():
+    query = db.session.query(Lab).join(Test, Lab.TCode == Test.TCode).filter(Test.TCode == 'GTT2').limit(1)
+    data = labs_schema.dumps(query)
+    return jsonify({'data': data})
+
+
+@apis.route('/tests/reflection')
+def get_test_reflection():
+    # test_table = db.Table('Test', db.metadata, autoload=True, autoload_with=db.engine)
+    return jsonify({'message': 'done', 'data': tests_schema.dumps(db.session.query(Test).all())})
+
 
 
 def generate(df):
