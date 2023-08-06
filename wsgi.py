649ad2d3e24b02a90eea8867f732337d7c129b72
Likit Preeyanon <likit.pre@mahidol.edu>
2023-08-06 16:51:21 +0700

    removed debug mode

--- wsgi.py
+++ wsgi.py
@@ -2,12 +2,10 @@ from app import create_app
 from app.apis import apis
 from waitress import serve
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
 app = create_app()
 
 app.register_blueprint(apis, url_prefix='/apis')
 
-if os.environ.get("FLASK_ENV") != "development":
-    serve(app, host='0.0.0.0', port=8080, threads=1)
+serve(app, host='0.0.0.0', port=8080, threads=1)
