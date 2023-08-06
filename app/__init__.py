5948f81ab576204e01bfaf7e8b88e6d433b04e8a
Likit Preeyanon <likit.pre@mahidol.edu>
2023-08-06 16:52:03 +0700

    reformatted code

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine
 
 engine = create_engine('mssql+pyodbc://10.4.101.16/cmsLIS?driver=SQL+Server+Native+Client+11.0')
 
+
 def create_app():
     app = Flask(__name__)
     return app
